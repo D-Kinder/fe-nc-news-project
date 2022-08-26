8f3cc41d7d3708526139d605e4f8ad347fc1afac
Darren Kinder <darrenkinder@darrens-mbp.home>
2022-08-26 14:37:25 +0100

    refactoring, rearrange of code, copy URL func

--- src/Components/ErrorComponent.js
+++ src/Components/ErrorComponent.js
@@ -6,38 +6,41 @@ const ErrorComponent = ({message, code}) => {
         return(
             <div className="not-found-page">
             <div className="not-found-message">
-                <h3>Oops, this page doesn't exist!</h3>
-                <p>It looks like you entered an invalid parameter, try entering a valid parameter or follow the links below to help you find what you're looking for!</p>
+                <h2>Oops, this page doesn't exist!</h2>
+                <br></br>
+                <p className="text-blue">It looks like you entered an invalid parameter, try entering a valid parameter or follow the links below to help you find what you're looking for!</p>
             </div>
+            <div className="not-found-links">
              <div className="not-found-articles">
              <Link to="/articles">
-             <h3>ARTICLES</h3>
+             <h4>ARTICLES</h4>
              <br></br>
-             <p>/articles/:article_id</p>
-             <p>Try: /articles/3</p>
+             <p className="text">/articles/:article_id</p>
+             <p className="text">Try: /articles/3</p>
              <br></br>
-             <p>/articles/:article_id/comments</p>
-             <p>Try: /articles/3/comments</p>
+             <p className="text">/articles/:article_id/comments</p>
+             <p className="text">Try: /articles/3/comments</p>
              </Link>
          </div>
          <div className="not-found-topics">
              <Link to="/topics">
-             <h3>TOPICS</h3>
+             <h4>TOPICS</h4>
              <br></br>
-             <p>/topics/:topic_slug</p>
+             <p className="text">/topics/:topic_slug</p>
              <br></br>
-             <p>Try: /topics/cooking or /topics/football</p>
+             <p className="text">Try: /topics/cooking or /topics/football</p>
              </Link>
          </div>
          <div className="not-found-users">
              <Link to="/users">
-             <h3>USERS</h3>
+             <h4>USERS</h4>
              <br></br>
-             <p>See all the amazing people that have contributed to the conversations at DK NC News</p>
+             <p className="text">See all the amazing people that have contributed to the conversations at DK NC News</p>
              </Link>
          </div>
          <div className="not-found-code">
-             <p>{message}</p>
+             <p className="text-blue">{message}</p>
+         </div>
          </div>
          </div>
         )
@@ -47,38 +50,41 @@ const ErrorComponent = ({message, code}) => {
         return(
             <div className="not-found-page">
             <div className="not-found-message">
-                <h3>Oops, this page doesn't exist!</h3>
-                <p>It looks like you entered a valid parameter but it doesn't exist in our database! Try a different parameter or follow the links below to help you find what you're looking for!</p>
+                <h2>Oops, this page doesn't exist!</h2>
+                <br></br>
+                <p className="text-blue">It looks like you entered an invalid parameter, try entering a valid parameter or follow the links below to help you find what you're looking for!</p>
             </div>
+            <div className="not-found-links">
              <div className="not-found-articles">
              <Link to="/articles">
-             <h3>ARTICLES</h3>
+             <h4>ARTICLES</h4>
              <br></br>
-             <p>/articles/:article_id</p>
-             <p>Try: /articles/3</p>
+             <p className="text">/articles/:article_id</p>
+             <p className="text">Try: /articles/3</p>
              <br></br>
-             <p>/articles/:article_id/comments</p>
-             <p>Try: /articles/3/comments</p>
+             <p className="text">/articles/:article_id/comments</p>
+             <p className="text">Try: /articles/3/comments</p>
              </Link>
          </div>
          <div className="not-found-topics">
              <Link to="/topics">
-             <h3>TOPICS</h3>
+             <h4>TOPICS</h4>
              <br></br>
-             <p>/topics/:topic_slug</p>
+             <p className="text">/topics/:topic_slug</p>
              <br></br>
-             <p>Try: /topics/cooking or /topics/football</p>
+             <p className="text">Try: /topics/cooking or /topics/football</p>
              </Link>
          </div>
          <div className="not-found-users">
              <Link to="/users">
-             <h3>USERS</h3>
+             <h4>USERS</h4>
              <br></br>
-             <p>See all the amazing people that have contributed to the conversations at DK NC News</p>
+             <p className="text">See all the amazing people that have contributed to the conversations at DK NC News</p>
              </Link>
          </div>
          <div className="not-found-code">
-            <p>{message}</p>
+             <p className="text-blue">{message}</p>
+         </div>
          </div>
          </div>
         )
--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -8,32 +8,34 @@ const NotFound = () => {
                 <h2>Oops, this page doesn't exist!</h2>
                 <p>Looks like you entered an incorrect pathway.  Follow the links below to the area you are looking for, or enter a URL in one of the forms below for something more specific</p>
             </div>
+            <div className="not-found-links">
             <div className="not-found-articles">
                 <Link to="/articles">
-                <h3>ARTICLES</h3>
+                <h4>ARTICLES</h4>
                 <br></br>
-                <p>/articles/:article_id</p>
-                <p>Try: /articles/3</p>
+                <p className="text">/articles/:article_id</p>
+                <p className="text">Try: /articles/3</p>
                 <br></br>
-                <p>/articles/:article_id/comments</p>
-                <p>Try: /articles/3/comments</p>
+                <p className="text">/articles/:article_id/comments</p>
+                <p className="text">Try: /articles/3/comments</p>
                 </Link>
             </div>
             <div className="not-found-topics">
                 <Link to="/topics">
-                <h3>TOPICS</h3>
+                <h4>TOPICS</h4>
                 <br></br>
-                <p>/topics/:topic_slug</p>
+                <p className="text">/topics/:topic_slug</p>
                 <br></br>
-                <p>Try: /topics/cooking or /topics/football</p>
+                <p className="text">Try: /topics/cooking or /topics/football</p>
                 </Link>
             </div>
             <div className="not-found-users">
                 <Link to="/users">
-                <h3>USERS</h3>
+                <h4>USERS</h4>
                 <br></br>
-                <p>See all the amazing people that have contributed to the conversations at DK NC News</p>
+                <p className="text">See all the amazing people that have contributed to the conversations at DK NC News</p>
                 </Link>
+            </div>
             </div>
         </div>
     )
